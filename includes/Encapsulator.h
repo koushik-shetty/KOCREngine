fb08f3af50474dc182e9853ed92d9b1dcf95d94e
Koushik Shetty <koushik@thoughtworks.com>
2016-01-10 22:40:34 +0530

    add char encapsulator

---
+++ includes/Encapsulator.h
@@ -0,0 +1,9 @@
+#ifndef __ENCAPSULATOR_H__
+#define __ENCAPSULATOR_H__ 
+
+class Encapsulator{
+public:
+	Encapsulator(const ImageReader& ir)
+	Encapsulator(ImageReader&& ir)
+};
+#endif //__ENCAPSULATOR_H__
---
+++ includes/OCR.h
@@ -0,0 +1,4 @@
+#ifndef __OCR_H__
+#define __OCR_H__
+
+#endif //__OCR_H__
